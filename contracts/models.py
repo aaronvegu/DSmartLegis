567973ce8c8522dc88c65c2a982fc162c7afcc9b
Aaron Vegu <aaron@reckoner-rde.io>
2023-04-28 23:54:13 -0600

    Contract n ContractType models added

--- contracts/models.py
+++ contracts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ContractType(models.Model):
+    type = models.CharField(max_length=150)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Contract(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    paid = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.ForeignKey(ContractType, on_delete=models.CASCADE)
